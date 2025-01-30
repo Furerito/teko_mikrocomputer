7ba50ba2da1cfe35373ac2053b143d2d5aba3498
David Furer <david.furer@srk-bern.ch>
2025-01-30 14:06:50 +0100

    led dimmer

---
+++ 06_led_dimmer/06_led_dimmer.ino
@@ -0,0 +1,55 @@
+const int led_upperEnd = 2;
+const int led_dimm = 3;
+const int led_lowerEnd = 4;
+const int button1 = 5;
+int dimm_led_power = 0;
+
+const int dimm_maxVal = 255;
+const int dimm_minVal = 0;
+bool dimmer_is_rising = true;
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(led_upperEnd, OUTPUT);
+  pinMode(led_dimm, OUTPUT);
+  pinMode(led_lowerEnd, OUTPUT);
+  pinMode(button1, INPUT_PULLUP);
+}
+
+void loop() {
+  buttonState = !digitalRead(button1);
+
+  Serial.println("Button " + String(buttonState) + " / Led Power " + String(dimm_led_power));
+
+  // Prüfe, ob der Button gedrückt ist
+  if (buttonState == HIGH) {  
+    if (dimmer_is_rising && dimm_led_power < dimm_maxVal) {
+      dimm_led_power++;
+    } else if (!dimmer_is_rising && dimm_led_power > dimm_minVal) {
+      dimm_led_power--;
+    }
+
+    if (dimm_led_power >= dimm_maxVal) {
+      dimmer_is_rising = false;
+    } else if (dimm_led_power <= dimm_minVal) {
+      dimmer_is_rising = true;
+    }
+  }
+
+  analogWrite(led_dimm, dimm_led_power);  // Korrigiert: LED dimmen mit PWM
+  if(dimm_led_power == dimm_minVal){
+    digitalWrite(led_lowerEnd, HIGH);
+    delay(1000);
+  }else{
+    digitalWrite(led_lowerEnd, LOW);
+  }
+  if(dimm_led_power == dimm_maxVal){
+    digitalWrite(led_upperEnd, HIGH);
+    delay(1000);
+  }else{
+    digitalWrite(led_upperEnd, LOW);
+  }
+  delay(10); // Kleiner Delay, um das Dimmen sanfter zu machen
+}
